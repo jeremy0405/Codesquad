d5f8519d791a78956c822ff2439b0b269dc493c2
hsjang <hsjang0405@gmail.com>
2022-01-03 17:26:05 +0900

    dec2bin 구현

--- src/mission/cs/cs01/Convertor.java
+++ src/mission/cs/cs01/Convertor.java
@@ -2,12 +2,25 @@ package mission.cs.cs01;
 
 public class Convertor {
 
-//	public boolean[] dec2bin(int decimal) {
-//
-//		boolean[] answer = new boolean[100];
-//
-//		return answer;
-//	}
+	public boolean[] dec2bin(int decimal) {
+
+		boolean[] tmp = new boolean[8];
+		int i = 0;
+		while(decimal != 0) {
+			if(decimal % 2 == 0) {
+				tmp[i] = false;
+			} else {
+				tmp[i] = true;
+			}
+			decimal = decimal / 2;
+			i++;
+		}
+		boolean[] answer = new boolean[i];
+
+		System.arraycopy(tmp, 0, answer, 0, answer.length);
+
+		return answer;
+	}
 
 	public int bin2dec(boolean[] bin) {
 		int answer = 0;
