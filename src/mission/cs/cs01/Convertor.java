4258babd206b74424735a88c6bbe0d35b9fd32e6
hsjang <hsjang0405@gmail.com>
2022-01-03 16:17:05 +0900

    bin2dec 구현

--- src/mission/cs/cs01/Convertor.java
+++ src/mission/cs/cs01/Convertor.java
@@ -1,2 +1,16 @@
-package mission.cs.cs01;public class Convertor {
+package mission.cs.cs01;
+
+public class Convertor {
+
+	public int bin2dec(boolean[] bin) {
+		int answer = 0;
+		int size = bin.length;
+		for (int i = 0; i < size; i++) {
+			if(bin[i]) {
+				answer += Math.pow(2, i);
+			}
+		}
+		return answer;
+	}
+
 }
