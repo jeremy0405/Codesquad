64b4b56655e03488eda0f0cbf43b303d4de7f63e
hsjang <hsjang0405@gmail.com>
2022-01-03 17:35:14 +0900

    메인클래스 convertor 이용해서 변경

--- src/mission/cs/cs01/Main.java
+++ src/mission/cs/cs01/Main.java
@@ -4,42 +4,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		Adder adder = new Adder();
+		Convertor convertor = new Convertor();
 
-		boolean[] byteA = {true, true, false, true, true, false, true, false};
-		boolean[] byteB = {true, false, true, true, false, false, true, true};
+		boolean[] byteA = convertor.dec2bin(24);
+		boolean[] byteB = convertor.dec2bin(40);
+		boolean[] answer = adder.upgradeByteadder(byteA, byteB);
 
-		boolean[] answer = adder.byteadder(byteA, byteB);
+		for (int i = 0; i < byteA.length; i++) {
+			System.out.print(byteA[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < byteB.length; i++) {
+			System.out.print(byteB[i] + " ");
+		}
+		System.out.println();
 		for (int i = 0; i < answer.length; i++) {
 			System.out.print(answer[i] + " ");
 		}
-
 		System.out.println();
+
+		System.out.println(
+			convertor.bin2dec(byteA) + " + " + convertor.bin2dec(byteB) + " = " + convertor.bin2dec(
+				answer));
 
 		for (boolean b : answer) {
-			if(b) {
+			if (b) {
 				System.out.print(1);
 			} else {
 				System.out.print(0);
 			}
 		}
 
-		System.out.println();
-		boolean[] byteC = {true, false, true, true};
-		boolean[] byteD = {true, true, false, true, true, false, true, false, true, true, false, true, true, false, true, true};
-
-		boolean[] answer2 = adder.upgradeByteadder(byteC, byteD);
-		for (boolean b : answer2) {
-			System.out.print(b + " ");
-		}
-
-		System.out.println();
-		for (boolean b : answer2) {
-			if(b) {
-				System.out.print(1);
-			} else {
-				System.out.print(0);
-			}
-		}
 	}
 
 }
