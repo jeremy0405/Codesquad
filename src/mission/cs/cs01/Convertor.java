39eb6194f333cd2e82ef98cb2fbdf7bbedd32a0d
hsjang <hsjang0405@gmail.com>
2022-01-03 16:32:12 +0900

    bin2dec Math.pow()제거 후 사칙연산으로 변경

--- src/mission/cs/cs01/Convertor.java
+++ src/mission/cs/cs01/Convertor.java
@@ -2,13 +2,22 @@ package mission.cs.cs01;
 
 public class Convertor {
 
+//	public boolean[] dec2bin(int decimal) {
+//
+//		boolean[] answer = new boolean[100];
+//
+//		return answer;
+//	}
+
 	public int bin2dec(boolean[] bin) {
 		int answer = 0;
+		int tmp = 1;
 		int size = bin.length;
 		for (int i = 0; i < size; i++) {
 			if(bin[i]) {
-				answer += Math.pow(2, i);
+				answer += tmp;
 			}
+			tmp = tmp * 2;
 		}
 		return answer;
 	}
