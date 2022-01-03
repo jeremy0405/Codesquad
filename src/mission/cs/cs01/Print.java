4845c771732f5e4b6b3a693d0f019d0e034ea317
hsjang <hsjang0405@gmail.com>
2022-01-03 18:09:24 +0900

    Print 클래스 생성

---
+++ src/mission/cs/cs01/Print.java
@@ -0,0 +1,29 @@
+package mission.cs.cs01;
+
+public class Print {
+
+	public void printArr(boolean[] arr) {
+		for (Object o : arr) {
+			System.out.print(o + " ");
+		}
+		System.out.println();
+	}
+
+	public void printToDec(boolean[] a, boolean[] b, boolean[] ab) {
+		Convertor convertor = new Convertor();
+		System.out.println(
+			convertor.bin2dec(a) + " + " + convertor.bin2dec(b) + " = " + convertor.bin2dec(
+				ab));
+	}
+
+	public void printBin(boolean[] bin) {
+		for (boolean b : bin) {
+			if (b) {
+				System.out.print(1);
+			} else {
+				System.out.print(0);
+			}
+		}
+	}
+
+}
