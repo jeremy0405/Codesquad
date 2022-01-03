4cab2f030bb5a0667cf4641d60c6cf64e694f045
hsjang <hsjang0405@gmail.com>
2022-01-03 18:10:00 +0900

    메인클래스에서 Print객체로 출력

--- src/mission/cs/cs01/Main.java
+++ src/mission/cs/cs01/Main.java
@@ -5,35 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		Adder adder = new Adder();
 		Convertor convertor = new Convertor();
+		Print print = new Print();
 
 		boolean[] byteA = convertor.dec2bin(24);
 		boolean[] byteB = convertor.dec2bin(40);
 		boolean[] answer = adder.upgradeByteadder(byteA, byteB);
 
-		for (int i = 0; i < byteA.length; i++) {
-			System.out.print(byteA[i] + " ");
-		}
-		System.out.println();
-		for (int i = 0; i < byteB.length; i++) {
-			System.out.print(byteB[i] + " ");
-		}
-		System.out.println();
-		for (int i = 0; i < answer.length; i++) {
-			System.out.print(answer[i] + " ");
-		}
-		System.out.println();
+		print.printToDec(byteA, byteB, answer);
 
-		System.out.println(
-			convertor.bin2dec(byteA) + " + " + convertor.bin2dec(byteB) + " = " + convertor.bin2dec(
-				answer));
+		print.printArr(byteA);
+		print.printArr(byteB);
+		print.printArr(answer);
 
-		for (boolean b : answer) {
-			if (b) {
-				System.out.print(1);
-			} else {
-				System.out.print(0);
-			}
-		}
+		print.printBin(answer);
 
 	}
 
