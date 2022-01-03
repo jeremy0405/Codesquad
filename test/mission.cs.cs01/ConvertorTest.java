7b75c84c47d02f40731aea3b3b985647b166a398
hsjang <hsjang0405@gmail.com>
2022-01-03 17:39:17 +0900

    dec2bin 테스트코드 작성

--- test/mission.cs.cs01/ConvertorTest.java
+++ test/mission.cs.cs01/ConvertorTest.java
@@ -2,6 +2,7 @@ package mission.cs.cs01;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,13 @@ import org.junit.jupiter.api.Test;
 class ConvertorTest {
 
 	Convertor convertor = new Convertor();
+
+	@Test
+	@DisplayName("dec2bin테스트")
+	void dec2binTest() {
+		boolean[] answer = {false, true, false, true};
+		Assertions.assertEquals(Arrays.equals(convertor.dec2bin(10), answer), true);
+	}
 
 	@Test
 	@DisplayName("bin2dec테스트")
