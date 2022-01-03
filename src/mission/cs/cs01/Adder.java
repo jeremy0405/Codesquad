83069f16822f090dafe5d75e3bdd56ad04457054
hsjang <hsjang0405@gmail.com>
2022-01-03 15:25:30 +0900

    byteadder 구현

--- src/mission/cs/cs01/Adder.java
+++ src/mission/cs/cs01/Adder.java
@@ -20,10 +20,26 @@ public class Adder {
 		boolean[] answer = new boolean[2];
 
 		boolean sum = halfadder(halfadder(bitA, bitB)[1], inputCarry)[1];
-		boolean carry = function.or(halfadder(halfadder(bitA, bitB)[1], inputCarry)[0], halfadder(bitA, bitB)[0]);
+		boolean carry = function.or(halfadder(halfadder(bitA, bitB)[1], inputCarry)[0],
+			halfadder(bitA, bitB)[0]);
 
 		answer[0] = carry;
 		answer[1] = sum;
+
+		return answer;
+	}
+
+	public boolean[] byteadder(boolean[] byteA, boolean[] byteB) {
+		boolean[] answer = new boolean[9];
+
+		boolean carry = false;
+
+		for (int i = 0; i < byteA.length; i++) {
+			boolean[] tmp = fulladder(byteA[i], byteB[i], carry);
+			answer[i] = tmp[1];
+			carry = tmp[0];
+		}
+		answer[8] = carry;
 
 		return answer;
 	}
