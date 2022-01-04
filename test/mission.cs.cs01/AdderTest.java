83a97a77ffb6e9cb9e01324757d82164b992d7dd
hsjang <hsjang0405@gmail.com>
2022-01-04 20:51:46 +0900

    halfadder fulladder private로 변경 후 테스트코드에서 제거

--- test/mission.cs.cs01/AdderTest.java
+++ test/mission.cs.cs01/AdderTest.java
@@ -11,36 +11,6 @@ class AdderTest {
 	Adder adder = new Adder();
 
 	@Test
-	@DisplayName("반쪽덧셈테스트")
-	void halfadder() {
-
-		Assertions.assertEquals(adder.halfadder(true, true)[0], true);
-		Assertions.assertEquals(adder.halfadder(true, true)[1], false);
-
-		Assertions.assertEquals(adder.halfadder(false, true)[0], false);
-		Assertions.assertEquals(adder.halfadder(false, true)[1], true);
-
-		Assertions.assertEquals(adder.halfadder(false, false)[0], false);
-		Assertions.assertEquals(adder.halfadder(false, false)[1], false);
-
-	}
-
-	@Test
-	@DisplayName("전체덧셈테스트")
-	void fulladder() {
-
-		Assertions.assertEquals(adder.fulladder(true, true, true)[0], true);
-		Assertions.assertEquals(adder.fulladder(true, true, true)[1], true);
-
-		Assertions.assertEquals(adder.fulladder(true, false, true)[0], true);
-		Assertions.assertEquals(adder.fulladder(true, false, true)[1], false);
-
-		Assertions.assertEquals(adder.fulladder(false, false, true)[0], false);
-		Assertions.assertEquals(adder.fulladder(false, false, true)[1], true);
-
-	}
-
-	@Test
 	@DisplayName("8비트덧셈기테스트")
 	void byteadder() {
 
