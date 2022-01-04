efe12fe76c6b5fabd4865d502cee1fda72199c5f
hsjang <hsjang0405@gmail.com>
2022-01-04 15:22:36 +0900

    dec2hex 리팩토링 switch문 분리, 배열 4의 배수길이로 분리

--- src/mission/cs/cs01/Convertor.java
+++ src/mission/cs/cs01/Convertor.java
@@ -6,8 +6,8 @@ public class Convertor {
 
 		boolean[] tmp = new boolean[8];
 		int i = 0;
-		while(decimal != 0) {
-			if(decimal % 2 == 0) {
+		while (decimal != 0) {
+			if (decimal % 2 == 0) {
 				tmp[i] = false;
 			} else {
 				tmp[i] = true;
@@ -27,7 +27,7 @@ public class Convertor {
 		int tmp = 1;
 		int size = bin.length;
 		for (int i = 0; i < size; i++) {
-			if(bin[i]) {
+			if (bin[i]) {
 				answer += tmp;
 			}
 			tmp = tmp * 2;
@@ -37,43 +37,53 @@ public class Convertor {
 
 	public String dec2hex(int decimal) {
 		boolean[] bin = dec2bin(decimal);
-		String answer = "";
+		StringBuilder answer = new StringBuilder();
 
 		boolean[] tmp;
 		for (int i = 0; i < bin.length; i += 4) {
-			if(i + 1 == bin.length) {
-				tmp = new boolean[]{bin[i], false, false, false};
-			} else if (i + 2 == bin.length) {
-				tmp = new boolean[]{bin[i], bin[i + 1], false, false};
-			} else if (i + 3 == bin.length) {
-				tmp = new boolean[]{bin[i], bin[i + 1], bin[i + 2], false};
-			} else {
-				tmp = new boolean[]{bin[i], bin[i + 1], bin[i + 2], bin[i + 3]};
-			}
-			switch (bin2dec(tmp)) {
-				case 10:
-					answer += "A";
-					break;
-				case 11:
-					answer += "B";
-					break;
-				case 12:
-					answer += "C";
-					break;
-				case 13:
-					answer += "D";
-					break;
-				case 14:
-					answer += "E";
-					break;
-				case 15:
-					answer += "F";
-					break;
-				default: answer += bin2dec(tmp);
-			}
+			tmp = setMultipleOfFour(bin, i);
+			bin2hexString(answer, tmp);
 		}
-		return answer;
+		return answer.toString();
+	}
 
+	private void bin2hexString(StringBuilder answer, boolean[] tmp) {
+		switch (bin2dec(tmp)) {
+			case 10:
+				answer.append("A");
+				break;
+			case 11:
+				answer.append("B");
+				break;
+			case 12:
+				answer.append("C");
+				break;
+			case 13:
+				answer.append("D");
+				break;
+			case 14:
+				answer.append("E");
+				break;
+			case 15:
+				answer.append("F");
+				break;
+			default:
+				answer.append(bin2dec(tmp));
+		}
+	}
+
+	private boolean[] setMultipleOfFour(boolean[] bin, int i) {
+		boolean[] tmp;
+		if (i + 1 == bin.length) {
+			tmp = new boolean[]{bin[i], false, false, false};
+		} else if (i + 2 == bin.length) {
+			tmp = new boolean[]{bin[i], bin[i + 1], false, false};
+		} else if (i + 3 == bin.length) {
+			tmp = new boolean[]{bin[i], bin[i + 1], bin[i + 2], false};
+		} else {
+			tmp = new boolean[]{bin[i], bin[i + 1], bin[i + 2], bin[i + 3]};
+		}
+		return tmp;
 	}
 
 }
