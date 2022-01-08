15855016b4506e3e8432c5059396bb82e5087d8d
hsjang <hsjang0405@gmail.com>
2022-01-08 20:22:18 +0900

    makeCSfile.sh로 테스트환경 구축 && myShell.sh로 서버로 전송

---
+++ src/mission/cs/cs02/shellscript/makeCSfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir day{1..16}
+
+for i in `seq 1 1 16`
+do
+	random_num="$(($RANDOM% 16))"
+	random_num=$(($random_num+1))
+	echo $random_num
+	random_file_name="$(($RANDOM% 10))"
+	random_file_name=$(($random_file_name+1))
+	touch day$random_num/$random_file_name.cs
+	touch day$i/$random_file_name.txt
+done
+
--- src/mission/cs/cs02/shellscript/myShell.sh
+++ src/mission/cs/cs02/shellscript/myShell.sh
@@ -4,22 +4,33 @@ if ! [ -d backup ]; then
 	mkdir backup
 fi
 
+echo ==============백업중===============
+
 for i in `seq 1 1 16`
 do
 	cd day$i
 
-	if [ -f *.cs ]; then
-		cp *.cs ../backup
+	csFiles=(`find . -name "*.cs"`)
+
+	if [ -n "${csFiles[0]}" ]; then
+		mkdir ../backup/day$i
+		cp *.cs ../backup/day$i
 	else
-		echo "day$i에는 cs 파일이 존재하지 않습니다."
+		echo day$i"에는 cs파일이 존재하지 않습니다."
 	fi
 
 	cd ..
+
 done
 
 today=$(date "+%Y%m%d")
 
-zip $today.zip backup/*
+zip $today.zip backup/* > /dev/null
 
-scp $today.zip jerry@192.168.19.128:~/backup/$today.zip
+scp $today.zip jerry@192.168.0.15:~/backup/$today.zip
 
+rm -rf backup
+
+rm $today.zip
+
+echo =============백업완료===============
