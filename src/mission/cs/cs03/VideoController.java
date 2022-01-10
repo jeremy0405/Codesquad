30a213158b56523c9694af39eac3146f5f6ad969
hsjang <hsjang0405@gmail.com>
2022-01-10 11:46:15 +0900

    VideoEditor를 관리하는 Controller 구현

---
+++ src/mission/cs/cs03/VideoController.java
@@ -0,0 +1,11 @@
+package mission.cs.cs03;
+
+public class VideoController {
+
+	public static void main(String[] args) {
+		VideoEditor videoEditor = new VideoEditor();
+		videoEditor.init();
+		videoEditor.start();
+	}
+
+}
