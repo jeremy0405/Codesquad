2ea6cfb0561be16d4cca13f4ef9d9db69706c08c
hsjang <hsjang0405@gmail.com>
2022-01-10 11:47:19 +0900

    Video정보를 담고 있는 VideoData 구현

---
+++ src/mission/cs/cs03/VideoData.java
@@ -0,0 +1,31 @@
+package mission.cs.cs03;
+
+import java.util.Random;
+
+public class VideoData {
+
+	private String id;
+	private String title;
+	private int runTime;
+	private int nextNode;
+	private Random random = new Random();
+
+	public VideoData(String title) {
+		this.id = IdCreator.getId();
+		this.title = title;
+		this.runTime = random.nextInt(15) + 1;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getRunTime() {
+		return runTime;
+	}
+
+}
