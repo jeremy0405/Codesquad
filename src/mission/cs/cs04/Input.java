e9bf9d551a34ea01b554b7c6a4ff54be3c688be2
hsjang <hsjang0405@gmail.com>
2022-01-13 14:19:44 +0900

    Input 클래스 설계 완료

---
+++ src/mission/cs/cs04/Input.java
@@ -0,0 +1,50 @@
+package mission.cs.cs04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Input{
+
+	private List<Point> points = new ArrayList<>();
+	Scanner sc = new Scanner(System.in);
+
+	public List<String> getUserInput() {
+		List<String> stringPointList = new ArrayList<>();
+		try {
+			StringTokenizer st = new StringTokenizer(sc.nextLine(), "-");
+			while (st.hasMoreTokens()) {
+				String tmp = st.nextToken();
+				stringPointList.add(tmp.substring(1, tmp.length() - 1));
+			}
+		} catch (Exception e) {
+			return getUserInput();
+		}
+		return stringPointList;
+	}
+
+	public List<Point> getPoints() {
+		InputValidation val = new InputValidation();
+		List<String> stringPointList = getUserInput();
+		for (String s : stringPointList) {
+			String[] xy = s.split(",");
+			points.add(new Point(Integer.parseInt(xy[0]), Integer.parseInt(xy[1])));
+		}
+
+		try {
+			points = val.validateInput(points);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			resetInput();
+		}
+
+		return points;
+	}
+
+	private void resetInput() {
+		points.clear();
+		getPoints();
+	}
+
+}
--- src/mission/cs/cs04/InputValidation.java
+++ src/mission/cs/cs04/InputValidation.java
@@ -5,12 +5,26 @@ import java.util.List;
 public class InputValidation {
 
 	public List<Point> validateInput(List<Point> points) throws Exception {
+
+		checkInputPair(points);
+
+		checkInputRange(points);
+
+		return points;
+	}
+
+	private void checkInputRange(List<Point> points) throws Exception {
 		for (Point point : points) {
 			if (point.getX() < 0 || point.getX() >= 25 || point.getY() < 0 || point.getY() >= 25) {
 				throw new Exception("유효하지 않은 값 범위입니다.");
 			}
 		}
-		return points;
+	}
+
+	private void checkInputPair(List<Point> points) throws Exception {
+		if (points.size() <= 1) {
+			throw new Exception("입력값은 2쌍 이상이여야 합니다.");
+		}
 	}
 
 }
