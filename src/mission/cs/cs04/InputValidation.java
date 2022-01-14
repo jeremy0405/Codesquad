07decd10b96e246fd21a1cbdaedf2fe5e2853c19
hsjang <hsjang0405@gmail.com>
2022-01-14 17:45:09 +0900

    약간의 리팩토링

--- src/mission/cs/cs04/InputValidation.java
+++ src/mission/cs/cs04/InputValidation.java
@@ -16,6 +16,9 @@ public class InputValidation {
 		for (Point point : points) {
 			if (point.getX() < 0 || point.getX() >= 25 || point.getY() < 0 || point.getY() >= 25) {
 				throw new Exception("유효하지 않은 값 범위입니다.");
+				//IllegalArgumentException
+				//IllegalStateException
+				//RuntimeException 상속받아서 커스텀 예외 해봐
 			}
 		}
 	}
--- src/mission/cs/cs04/Point.java
+++ src/mission/cs/cs04/Point.java
@@ -2,8 +2,8 @@ package mission.cs.cs04;
 
 public class Point {
 
-	private int x;
-	private int y;
+	private final int x;
+	private final int y;
 
 	public Point(int x, int y) {
 		this.x = x;
