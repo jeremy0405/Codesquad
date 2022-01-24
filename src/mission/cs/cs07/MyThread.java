baa0372c3f2c2d876d0a82eed2735ece892cfc70
hsjang <hsjang0405@gmail.com>
2022-01-24 14:55:20 +0900

    ♻️  suspend, start, stop, resume 직접 구현

--- src/mission/cs/cs07/MyThread.java
+++ src/mission/cs/cs07/MyThread.java
@@ -1,17 +1,47 @@
 package mission.cs.cs07;
 
-public class MyThread extends Thread {
+public class MyThread implements Runnable {
+
+	volatile boolean suspended; //volatile -> '쉽게 바뀌는 변수'선언해야 쓰레드종료
+	volatile boolean stopped;
+	private final Thread thread;
+
+	public MyThread() {
+		this.suspended = false;
+		this.stopped = false;
+		this.thread = new Thread(this);
+	}
 
 	@Override
 	public void run() {
 		int i = 0;
-		while (true) {
-			//todo do nothing
-			i++;
-			if (i % 300000000 == 0) {
-				System.out.println(i);
+		while (!stopped) {
+			if (!suspended) {
+				i++;
+				if (i % 300000000 == 0) {
+					System.out.println(i);
+				}
+			} else {
+				Thread.yield();
 			}
 		}
+	}
 
+	public void suspend() {
+		suspended = true;
+		thread.interrupt();
+	}
+
+	public void resume() {
+		suspended = false;
+	}
+
+	public void stop() {
+		stopped = true;
+		thread.interrupt();
+	}
+
+	public void start() {
+		thread.start();
 	}
 }
