d022d43453d79bc307391968d0cd72e62d18b13b
hsjang <hsjang0405@gmail.com>
2022-01-27 15:40:58 +0900

    ✨  java8 practice

--- src/mission/cs/cs08/java8study/App.java
+++ src/mission/cs/cs08/java8study/App.java
@@ -7,13 +7,26 @@ public class App {
 
 	public static void main(String[] args) {
 		ExecutorService executorService = Executors.newSingleThreadExecutor();
-		executorService.execute(new Runnable() {
-			@Override
-			public void run() {
-				System.out.println(Thread.currentThread().getName());
-			}
-		});
 
+
+		executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+		executorService.shutdown();
+
+//		executorService.submit(() -> System.out.println(Thread.currentThread().getName()));
+//		executorService.shutdown();
+
+		ExecutorService executorService2 = Executors.newFixedThreadPool(2);
+		executorService2.submit(getRunnable());
+
+		executorService2.shutdown();
+
+
+	}
+
+	private static Runnable getRunnable() {
+		return () -> {
+			System.out.println("Thread : " + Thread.currentThread().getName());
+		};
 	}
 
 }
