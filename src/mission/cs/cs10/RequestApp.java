f934744ca1fe5185d822ab180287cbe70b5740c4
hsjang <hsjang0405@gmail.com>
2022-02-11 00:24:15 +0900

    ✨  requset 구현중..

--- src/mission/cs/cs10/RequestApp.java
+++ src/mission/cs/cs10/RequestApp.java
@@ -1,33 +1,80 @@
 package mission.cs.cs10;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class RequestApp {
 
 	private Socket socket;
+	private InetAddress byName;
 
 	public RequestApp(String url) {
 
 		try {
 			URL dns = getURL(url);
-//			System.out.println(this.url.getHost());
-//			System.out.println(this.url.getProtocol());
-//			System.out.println(this.url.getDefaultPort());
 			connectTCP(dns);
+			request(dns);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 	}
 
+	private void request(URL dns) throws IOException {
+
+		socket.setSoTimeout(3000);
+
+		BufferedWriter out = new BufferedWriter(
+			new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+		BufferedReader in = new BufferedReader(
+			new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+
+		List<String> headers = new ArrayList<>();
+
+		headers.add("GET" + getQuery(dns) + "HTTP/1.1");
+//		headers.add("Host: " + dns.getHost());
+//		headers.add("Accept-Language: en-US,en;q=0.9");
+//		headers.add("Accept-Encoding: gzip, deflate, br");
+
+		System.out.println("\n=======Request=======");
+
+		for (String header : headers) {
+			System.out.println(header);
+			out.write(header + "\r\n");
+		}
+
+		out.write("\r\n");
+
+		out.flush();
+		System.out.println("\n=======Response=======");
+
+		String line;
+		while ((line = in.readLine()) != null) {
+			System.out.println(line);
+		}
+
+	}
+
+	private String getQuery(URL dns) {
+		if (Objects.isNull(dns.getQuery())) {
+			return " ";
+		}
+		return " " + dns.getPath() + "?" + dns.getQuery() + " ";
+	}
+
 	private void connectTCP(URL url) throws IOException {
-		InetAddress byName = InetAddress.getByName(url.getHost());
+		byName = InetAddress.getByName(url.getHost());
 		System.out.println("(DNS Lookup...)");
 		socket = new Socket(getHostAddress(byName), getPort(url));
 		System.out.println("TCP Connection : " + getHostAddress(byName) + " " + getPort(url));
